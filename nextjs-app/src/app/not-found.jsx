913dd0ec3fbf050e464bb9e3ce7fe5f14641a7c1
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-29 12:58:07 +0600

    add loading and not found components

---
+++ nextjs-app/src/app/not-found.jsx
@@ -0,0 +1,9 @@
+export default function Notfound() {
+  return (
+    <div className="flex flex-col items-center py-20">
+      <h2>Not Found</h2>
+      <p>Could not find requested resource</p>
+      <button className="btn">Back to Home</button>
+    </div>
+  );
+}
