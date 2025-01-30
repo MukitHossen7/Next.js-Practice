3702879e65c28a2d0fbf5bb13f6421eacfb7ccfe
MukitHossen7 <hossenmukit7@gmail.com>
2025-01-30 21:15:36 +0600

    conditionally render footer based on pathname

--- nextjs-app/src/app/components/Footer.jsx
+++ nextjs-app/src/app/components/Footer.jsx
@@ -1,9 +1,18 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
 const Footer = () => {
-  return (
-    <div className="bg-gray-800 text-gray-200 py-4 text-center">
-      <p>�� 2023 My Website. All rights reserved.</p>
-    </div>
-  );
+  const pathname = usePathname();
+  if (!pathname.includes("dashboard")) {
+    return (
+      <div className="bg-gray-800 text-gray-200 py-4 text-center">
+        <p>�� 2023 My Website. All rights reserved.</p>
+      </div>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Footer;
