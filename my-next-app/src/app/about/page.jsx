71c9103c0f3239d5ffc5de5ae999a13479e0f52b
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-21 16:36:02 +0600

    add About and Home pages as placeholders

---
+++ my-next-app/src/app/about/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">About</h2>
+      <p className="mt-2">
+        Welcome to the About page. This is a simple page that does not have any
+        functionality. It is just a placeholder for the About page.
+      </p>
+    </div>
+  );
+};
+
+export default About;
---
+++ my-next-app/src/app/home/page.jsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">Home</h2>
+      <p className="mt-2">
+        Welcome to the Home page. This is a simple page that does not have any
+        functionality. It is just a placeholder for the Home page.
+      </p>
+    </div>
+  );
+};
+
+export default Home;
