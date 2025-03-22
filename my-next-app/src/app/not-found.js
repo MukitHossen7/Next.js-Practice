c26ac7c448fd09276ca1cf7f25e9e4419e1f4f7c
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-22 11:25:39 +0600

    add NotFound component for 404 error handling; remove unnecessary line in Details page

---
+++ my-next-app/src/app/not-found.js
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1>404 - Not Found</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+      <button className="bg-blue-700 px-4 py-2">Go Back Home</button>
+    </div>
+  );
+}
