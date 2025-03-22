620ea2f81907b4b8326fab94d4edafbd6ba9a11f
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-22 23:12:10 +0600

    add Roboto font to Home page for improved typography

--- my-next-app/src/app/home/page.jsx
+++ my-next-app/src/app/home/page.jsx
@@ -1,6 +1,12 @@
+import { Roboto } from "next/font/google";
+const roboto = Roboto({
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["100", "300", "400", "500", "700", "900"],
+});
 const Home = () => {
   return (
-    <div className="main">
+    <div className={`main ${roboto.className}`}>
       <h2 className="text-2xl font-bold">Home</h2>
       <p className="mt-2">
         Welcome to the Home page. This is a simple page that does not have any
