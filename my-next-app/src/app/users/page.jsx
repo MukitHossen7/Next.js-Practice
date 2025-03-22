6890c0700efb94e395693306cf3006364b7a9baf
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-22 22:21:28 +0600

    add image support to Users page and configure image domains in Next.js

--- my-next-app/src/app/users/page.jsx
+++ my-next-app/src/app/users/page.jsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import img1 from "../Images/images (27).jpg";
+
 async function userData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
@@ -10,6 +13,14 @@ const Users = async () => {
       <h2 className="text-2xl text-center py-2">
         Fetch Data and Server Side Rendering
       </h2>
+      <Image src={img1} alt="Hile Images" width="500" height="500"></Image>
+      <Image
+        src="https://images.pexels.com/photos/13003306/pexels-photo-13003306.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+        alt="pexels Images"
+        width="800"
+        height="500"
+        style={{ objectFit: "cover" }}
+      ></Image>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
         {users.map((user) => (
           <div
