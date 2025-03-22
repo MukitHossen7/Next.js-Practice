f6cfd234df9db9df10fb067cd84dc3cb9f64cbe1
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-22 16:36:18 +0600

    add styling for Users page and update layout for user cards

--- my-next-app/src/app/users/page.jsx
+++ my-next-app/src/app/users/page.jsx
@@ -1,3 +1,4 @@
+import "./style.css";
 async function userData() {
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await response.json();
@@ -6,13 +7,16 @@ async function userData() {
 const Users = async () => {
   const users = await userData();
   return (
-    <div>
+    <div className="">
       <h2 className="text-2xl text-center py-2">
         Fetch Data and Server Side Rendering
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 ">
         {users.map((user) => (
-          <div key={user.id} className="bg-gray-100 p-2 my-2 text-gray-900">
+          <div
+            key={user.id}
+            className="bg-gray-100 p-2 my-2 text-gray-900 mainDiv"
+          >
             <h3 className="text-xl">{user.name}</h3>
             <p>{user.email}</p>
           </div>
