a0354b79b299b8f5c7d44130cd09d9b94785cbdf
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-22 11:30:05 +0600

    add HomeNotFoundPage component for 404 error handling in Home section

---
+++ my-next-app/src/app/home/[...notFoundPage]/page.jsx
@@ -0,0 +1,10 @@
+const HomeNotFoundPage = () => {
+  return (
+    <div className="container mx-auto p-4">
+      <h1>404 - Not Found in Home page</h1>
+      <p>Sorry, the page you are looking for does not exist.</p>
+    </div>
+  );
+};
+
+export default HomeNotFoundPage;
