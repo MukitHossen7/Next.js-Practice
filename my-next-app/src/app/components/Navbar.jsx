1426d36c9375670b009c8c8db68fbeb612391495
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-25 14:51:09 +0600

    add link to Post page in Navbar component

--- my-next-app/src/app/components/Navbar.jsx
+++ my-next-app/src/app/components/Navbar.jsx
@@ -10,6 +10,7 @@ const Navbar = () => {
         <Link href="/blog">Blog</Link>
         <Link href="/users">Users</Link>
         <Link href="/add_post">Add Post</Link>
+        <Link href="/post">Post</Link>
       </nav>
     </>
   );
