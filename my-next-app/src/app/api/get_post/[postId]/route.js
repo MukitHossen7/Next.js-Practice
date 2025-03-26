f7ca053fb24f97a09c95f5bbab264666a35754c0
MukitHossen7 <hossenmukit7@gmail.com>
2025-03-26 11:00:17 +0600

    add GET and PUT API routes for retrieving and updating blog posts by ID

--- my-next-app/src/app/api/get_post/[postId]/route.js
+++ my-next-app/src/app/api/get_post/[postId]/route.js
@@ -4,10 +4,18 @@ import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
 export const PUT = async (req, res) => {
-  const idValue = res.params.putId;
+  const idValue = res.params.postId;
   const id = { _id: idValue };
   const payload = await req.json();
   await mongoose.connect(connectionString);
   const updatePost = await BlogPostModel.findOneAndUpdate(id, payload);
   return NextResponse.json({ success: true });
+};
+
+export const GET = async (req, res) => {
+  const getId = res.params.postId;
+  const id = { _id: getId };
+  await mongoose.connect(connectionString);
+  const getPost = await BlogPostModel.findById(id);
+  return NextResponse.json(getPost);
 };
