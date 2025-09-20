1f49639dbb28b43aff09db54305bc767c9d6c1db
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-20 10:26:16 +0600

    use Image component for optimize the images

--- next-app/src/app/(commonLayout)/blog/page.tsx
+++ next-app/src/app/(commonLayout)/blog/page.tsx
@@ -1,9 +1,32 @@
-import style from "./blog.module.css";
-
+import Image from "next/image";
+import blogImg from "../../../assets/image2.jpg";
 const BlogPage = () => {
   return (
     <div>
-      <h1 className={style.text_style}>This is Blog Page</h1>
+      <h1 className="text-4xl text-center">This is Blog Page</h1>
+      <Image
+        src="https://media.istockphoto.com/id/887987150/photo/blogging-woman-reading-blog.jpg?s=612x612&w=0&k=20&c=7SScR_Y4n7U3k5kBviYm3VwEmW4vmbngDUa0we429GA="
+        alt="remote image"
+        width={500}
+        height={500}
+        className="mx-auto"
+      />
+
+      <Image
+        src="/image2.jpg"
+        alt="remote image"
+        width={500}
+        height={500}
+        className="mx-auto mt-2"
+      />
+
+      <Image
+        src={blogImg}
+        alt="remote image"
+        width={500}
+        height={500}
+        className="mx-auto mt-2"
+      />
     </div>
   );
 };
