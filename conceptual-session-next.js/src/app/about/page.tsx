04e71696b199945fcf08a55d8089e35079b5e890
MukitHossen7 <hossenmukit7@gmail.com>
2025-09-23 23:10:33 +0600

    some update

--- conceptual-session-next.js/src/app/about/page.tsx
+++ conceptual-session-next.js/src/app/about/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 const AboutPage = () => {
   return (
     <div>
       <h1 className="text-4xl text-center">This About Page</h1>
+      <Link href="/intercepting">
+        <button>Click Me</button>
+      </Link>
     </div>
   );
 };
---
+++ conceptual-session-next.js/src/app/intercepting/page.tsx
@@ -0,0 +1,12 @@
+const InterceptingPage = () => {
+  return (
+    <div>
+      <h1 className="text-3xl mb-10">This is Interception page</h1>
+      <div className="w-32 h-32 bg-amber-400 border-gray-100 flex justify-center items-center text-black text-4xl">
+        1
+      </div>
+    </div>
+  );
+};
+
+export default InterceptingPage;
